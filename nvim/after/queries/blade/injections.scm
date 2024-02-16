9924064a7b74b2a13394ef1fc41ac32911bbb3e2
kleninmaxim <maks.klen.99@gmail.com>
2024-02-16 21:24:29 +0300

    Update lazy and change blade injections

--- nvim/after/queries/blade/injections.scm
+++ nvim/after/queries/blade/injections.scm
@@ -1,7 +1,4 @@
-((php) @injection.content
-    (#set! injection.language "php")
-    (#set! injection.combined))
-((parameter) @injection.content
-    (#set! injection.language "php")) ; TODO: php_only
-((php_only) @injection.content
-    (#set! injection.language "php")) ; TODO: php_only
+((text) @injection.content
+    (#not-has-ancestor? @injection.content "envoy")
+    (#set! injection.combined)
+    (#set! injection.language php))
