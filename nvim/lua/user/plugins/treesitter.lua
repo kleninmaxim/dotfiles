fc7e997908010aed7d0a9d04ed4a80e9c2b03385
kleninmaxim <maks.klen.99@gmail.com>
2024-02-16 21:28:14 +0300

    Fix blade treesitter

--- nvim/lua/user/plugins/treesitter.lua
+++ nvim/lua/user/plugins/treesitter.lua
@@ -82,22 +82,22 @@ return {
       },
     },
   },
-  -- config = function (_, opts)
-  --   require('nvim-treesitter.configs').setup(opts)
+  config = function (_, opts)
+    require('nvim-treesitter.configs').setup(opts)
 
-  --   local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
-  --   parser_config.blade = {
-  --     install_info = {
-  --       url = "https://github.com/EmranMR/tree-sitter-blade",
-  --       files = {"src/parser.c"},
-  --       branch = "main",
-  --     },
-  --     filetype = "blade"
-  --   }
-  --   vim.filetype.add({
-  --     pattern = {
-  --       ['.*%.blade%.php'] = 'blade',
-  --     },
-  --   })
-  -- end,
+    local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+    parser_config.blade = {
+      install_info = {
+        url = "https://github.com/EmranMR/tree-sitter-blade",
+        files = {"src/parser.c"},
+        branch = "main",
+      },
+      filetype = "blade"
+    }
+    vim.filetype.add({
+      pattern = {
+        ['.*%.blade%.php'] = 'blade',
+      },
+    })
+  end,
 }
