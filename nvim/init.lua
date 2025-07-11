463724e3d065a983feeb3aead9f8de03b6842b01
kleninmaxim <maks.klen.99@gmail.com>
2025-07-11 20:26:44 +0300

    Refactor dotfiles setup by removing unused configurations and files, including Neovim, Kitty, and Tmux settings. Update Git configuration to include LFS filters and add .vscode to global gitignore. Clean up Zsh configuration and remove obsolete snippets and plugin files.

--- nvim/init.lua
+++
@@ -1,4 +0,0 @@
-require('user.options')
-require('user.keymaps')
-require('user.misc')
-require('user.plugins')
--- nvim/lua/user/plugins/vim-mundo.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'simnalamburt/vim-mundo',
-  keys = {
-    { '<leader>K', ':MundoToggle<CR>' },
-  },
-  config = function()
-    vim.g.mundo_width = 90
-  end,
-}
